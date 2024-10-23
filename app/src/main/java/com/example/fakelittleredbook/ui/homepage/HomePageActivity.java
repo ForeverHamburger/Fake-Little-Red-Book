6b27211b8a0ac09a650cf803ba1f66477ac911e6
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2024-10-23 21:53:14 +0800

    首次提交，做了入场动画

---
+++ app/src/main/java/com/example/fakelittleredbook/ui/homepage/HomePageActivity.java
@@ -0,0 +1,51 @@
+package com.example.fakelittleredbook.ui.homepage;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fakelittleredbook.R;
+import com.example.fakelittleredbook.databinding.ActivityHomePageBinding;
+import com.google.android.material.tabs.TabLayout;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomePageActivity";
+    ActivityHomePageBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.bottomTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
